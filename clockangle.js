5b32e2beb1aa7305901392eb9d93594b3fc8ef42
Greg Domorski <greg.domorski@gmail.com>
2015-06-18 01:53:10 -0400

    Clock Angle Problem

---
+++ clockangle.js
@@ -0,0 +1,48 @@
+/*
+
+* Notes
+There is 360 degrees in a circle or in this case the clock
+There are 12 hours in a day so each hour is 30 degrees(360/12)
+A clock has 60 minute marks
+When the minute hand moves the hour hands move with it
+If the hour hand has to move 30 degrees in 60 minutes, 30/60 = .5 degrees each minute
+
+* Step 1
+First, find the angle of the the hour hand
+The hour times 30(the degrees between each hour), and add .5 for each minute
+
+* Step 2
+Second, find the angle of the minute hand
+If we know the clock moves 30 degrees for each hour, it also moves 30 degrees every five minutes(six degrees a minute) (minutes / 6) * 30
+
+* Step 3
+Sometimes the hour minute hand passes by the hour hand and other times it does not, so find if it passed it or not and subtract larger angle from smaller
+
+* Step 4 
+At some point the hands will be touching together and there will be no angle, 12 hours in a day, so after 12 o'clock, the hands will touch 11 times
+
+*/
+
+
+
+
+function findAngle(hour, minute){
+	var hourHand = Math.abs((hour * 30) + 0.5 * minute);  //Step1
+	var minutesHand = Math.abs((minute/6) * 30); //Step2
+	
+	if((hourHand === 360 && minutesHand === 0) || (hourHand === minutesHand)){   //Step 4
+	    return 0;
+	}
+
+	var angle1 = hourHand - minutesHand; //Step 3 
+	var angle2 = minutesHand - hourHand;
+
+	if(angle1>angle2){  //which angle is bigger, or are they equal?
+		return angle1;
+	}else{
+		return angle2;
+
+	}
+
+}
+}
