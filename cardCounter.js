34286de0137c05f97d45ee735bfee491990eee86
Greg Domorski <greg.domorski@gmail.com>
2015-07-13 16:57:29 -0400

    Update cardCounter.js

--- cardCounter.js
+++ cardCounter.js
@@ -2,17 +2,27 @@
 
 function cardCounter(arr){
     
+    //create an array of all the cards that subtract one from the counter
     var minusOne = [10, 'J', 'Q', 'K', "A"];
+    //create an array of all the cards that add one from the counter
     var plusOne = [1,2,3,4,5,6];
+    //set the count to zero
     var count = 0;
     
+    //loop through the array
     for(var i = 0; i < arr.length; i++){
+        //if it is in the plush one deck
         if(plusOne.indexOf(arr[i]) !== -1){
-         count++;
+        //add to the count
+        count++;
+        //or else if it is in the minus one deck
         }else if(minusOne.indexOf(arr[i]) != -1){
-         count--;   
+        //subtract from the count
+        count--;   
         }
     }
+    
+    //return the count
     return count;
 }
 
